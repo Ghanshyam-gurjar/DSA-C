c6a8ddb0c183a187fddee6f1dd6ae8e6998799ed
ghanshyam <ghanshyamgurjar223@gmail.com>
2026-05-21 07:03:57 +0530

    i bubble sort code

--- arrays/bubble-sort.c
+++ arrays/bubble-sort.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+int main(){
+int a[]={6,1,5,3,15,9};
 
+int i, j,n=6,c;
+
+for (j=0;j<(n-1); ++j)
+{
+
+
+for (i=0;i<(n-1);++i)
+
+if (a[i] > a[i+1])
+{
+
+c=a[i];
+a[i]=a[i+1];
+a[i+1]=c;
+
+
+}
+}
+for(i=0;i<n; i++)
+printf("%d ",a[i]);
+
+
+}
